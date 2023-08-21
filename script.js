c4dd656f1c7299fedebc0f491a95f0d902a0b0ce
Wojciech Nowak <wojtekn00@gmail.com>
2023-08-21 14:54:56 +0200

    update gallery section, user can now change width and see more photos in gallery

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const galleryContainer = document.getElementById('gallery');
+
+const expandGalleryButton = document.getElementById('expand-gallery');
+
+expandGalleryButton.addEventListener('click', () => {
+  galleryContainer.style.height = '200vh';
+});
